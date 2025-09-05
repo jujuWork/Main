49d59fab76d4213717e00f0d7597b56364bc4911
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-09-05 10:09:49 +0900

    drumKit

--- Course_Project/drum/index.js
+++ Course_Project/drum/index.js
@@ -3,6 +3,38 @@ let numberOfDrumButton = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumButton; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+
+        let buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+        case "w":
+            audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            audio = new Audio("sounds/snare.mp3");
+            audio.play();
+          default:
+            break;
+        }
     });
 }
