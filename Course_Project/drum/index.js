e46dff887a4d2ee82272264d1c0e39d8b6df033b
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-09-05 15:00:35 +0900

    drumkitSound

--- Course_Project/drum/index.js
+++ Course_Project/drum/index.js
@@ -1,40 +1,56 @@
 let numberOfDrumButton = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButton; i++) {
-
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
         let buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-        case "w":
-            audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-        case "a":
-            audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        case "s":
-            audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-        case "d":
-            audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
-        case "j":
-            audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-        case "k":
-            audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-        case "l":
-            audio = new Audio("sounds/snare.mp3");
-            audio.play();
-          default:
-            break;
-        }
+        makeSound(buttonInnerHTML);
     });
+
+    // Keydown listener
+    document.addEventListener("keydown", function(event) {
+        makeSound(event.key);
+    });
+
+    // Function to play sounds on keyboard press
+    function makeSound(key) {
+
+        switch (key) {
+            case "w":
+                audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                
+            default:
+                break;
+            }
+    }
 }
