afa99a587c2e0fa42fb460cac894c87d6fc04ab6
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-09-09 10:59:38 +0900

    click sound

--- Course_Project/SimonGame/index.js
+++ Course_Project/SimonGame/index.js
@@ -2,6 +2,16 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+$(".btn").click(function () {
+    let userChosenColor = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+});
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * buttonColors.length);
 
@@ -9,10 +19,14 @@ function nextSequence() {
 
     gamePattern.push(randomChosenColor);
 
-        // jQuery to choose a random color from the ID of the button color and fade IN and Out
+        // jQuery to choose a random color from the ID of the button color to fade IN and Out
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    playSound(randomChosenColor);
+};
+
+function playSound(name) {
         // Javascript to play Audio the random Chosen Color that is linked to the buttonColor array
-    let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
