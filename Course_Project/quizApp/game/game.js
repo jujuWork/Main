0d5b45097288bfc7b02ee4df43d251bcf0b935a9
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-09-14 22:44:07 +0900

    startGame JS

---
+++ Course_Project/quizApp/game/game.js
@@ -0,0 +1,61 @@
+const question = document.getElementById("question");
+const choice = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Inside which HTML element do we put the Javascript",
+            choice1: "<script>",
+            choice2: "<javascript>",
+            choice3: "<js>",
+            choice4: "<scripting>",
+            answer: 1
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+            choice1: "<script href = 'xxx.js'>",
+            choice2: "<script name = 'xxx.js'>",
+            choice3: "<script src = 'xxx.js'>",
+            choice4: "<script file = 'xxx.js'>",
+            answer: 3
+    },
+    {
+        question: "How do you write 'Hello World' in the alert box?",
+            choice1: "msgBox('Hello World')",
+            choice2: "alertBox('Hello World')",
+            choice3: "msg('Hello World')",
+            choice4: "alert('Hello World')",
+            answer: 4
+    },
+];
+
+const CORRECt_BONUS = 10;
+const MAX_QUESTION = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ... questions]; // wrapping all question and adding it into available questions
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choice.forEach( choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+};
+
+
+startGame();
